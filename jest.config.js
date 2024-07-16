c7e724b50cc3e3ca3098ede35c5f68d916c53758
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-16 19:26:09 -0300

    style: remove unecessary comments

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/en/configuration.html
- */
-
 module.exports = {
   roots: ["<rootDir>/src"],
   collectCoverageFrom: ["<rootDir/src/**/*.ts>"],
