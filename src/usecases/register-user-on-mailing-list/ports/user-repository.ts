8999349fcaf1bf5dc204505c5d53c36ac45436d2
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-17 13:15:37 -0300

    feat: add three new methods add,exists, and findUserByEmail

--- src/usecases/register-user-on-mailing-list/ports/user-repository.ts
+++ src/usecases/register-user-on-mailing-list/ports/user-repository.ts
@@ -1,5 +1,7 @@
 import { UserData } from '../user-data'
 
 export interface UserRepository {
+  add(user: UserData): Promise<void>;
   findUserByEmail(email: string): Promise<UserData>;
+  exists(user: UserData): Promise<boolean>;
 }
