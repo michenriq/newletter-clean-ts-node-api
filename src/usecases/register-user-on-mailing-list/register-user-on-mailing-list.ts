fbeba939dda885625d4d02baad5645f59dccee4a
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-18 16:49:13 -0300

    feat: register valid user

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -0,0 +1,26 @@
+import { InvalidEmailError } from '../../entities/errors/invalid-email-error'
+import { InvalidNameError } from '../../entities/errors/invalid-name-error'
+import { UserData } from '../../entities/protocols/user-data'
+import { User } from '../../entities/user'
+import { UserRepository } from './ports/user-repository'
+import { Either, left, right } from '../../shared'
+
+export class RegisterUserOnMailingList {
+    private readonly userRepository: UserRepository
+    constructor (userRepository: UserRepository) {
+      this.userRepository = userRepository
+    }
+
+    public async execute (request: UserData): Promise<Either<InvalidEmailError | InvalidEmailError, UserData>> {
+      const userOrError: Either<InvalidEmailError | InvalidNameError, User> = User.create(request)
+
+      if (userOrError.isLeft()) {
+        return left(userOrError.value)
+      }
+      if (!(await this.userRepository.exists(request))) {
+        await this.userRepository.add(request)
+
+        return right(request)
+      }
+    }
+}
