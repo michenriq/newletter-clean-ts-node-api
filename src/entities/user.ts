948ccf5447110faeba6e015427f657564ec5e87c
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-18 10:06:45 -0300

    feat: verify e-mail address when creating a user

---
+++ src/entities/user.ts
@@ -0,0 +1,13 @@
+import { Either, left } from '../shared'
+import { Email } from './email'
+import { InvalidEmailError } from './errors/invalid-email-error'
+import { UserData } from './protocols/user-data'
+
+export class User {
+  static create (userData: UserData): Either<InvalidEmailError, User> {
+    const emailOrError = Email.create(userData.email)
+    if (emailOrError.isLeft()) {
+      return left(new InvalidEmailError())
+    }
+  }
+}
