a5c41732328709f036528f03071d31c68e351bc0
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-18 16:53:56 -0300

    test: prevent system from adding user with invalid email

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,4 +1,6 @@
+import { InvalidEmailError } from '../../entities/errors/invalid-email-error'
 import { UserData } from '../../entities/protocols/user-data'
+import { left } from '../../shared'
 import { UserRepository } from './ports/user-repository'
 import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
 import { InMemoryUserRepository } from './repository/in-memory-user-repository'
@@ -22,5 +24,24 @@ describe('Register user on mailist list', () => {
 
     expect((await user).name).toBe('any_name')
     expect(response.value.name).toBe('any_name')
+  })
+  test('should add user with invalid email to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
+      repo
+    )
+    const name = 'any_name'
+    const invalidEmail = 'any.com'
+
+    const response = await useCase.execute({
+      name,
+      email: invalidEmail
+    })
+    const user = await repo.findUserByEmail(invalidEmail)
+
+    expect(user).toBeNull()
+    expect(response).toEqual(left(new InvalidEmailError()))
   })
 })
