2a7b44a24399a71ec5cfffd6ffafeb7b95364a10
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-18 10:25:25 -0300

    feat: add name validation to the user class

---
+++ src/entities/name.ts
@@ -0,0 +1,35 @@
+import { Either, left, right } from '../shared'
+import { InvalidNameError } from './errors/invalid-name-error'
+
+export class Name {
+    public readonly value: string
+
+    private constructor (name: string) {
+      this.value = name
+    }
+
+    public static create (name: string): Either<InvalidNameError, Name> {
+      if (Name.validate(name)) {
+        return right(new Name(name))
+      }
+
+      return left(new InvalidNameError())
+    }
+
+    public static validate (name: string): boolean {
+      const trimmedName = name.trim()
+      if (!trimmedName) {
+        return false
+      }
+
+      if (trimmedName.length < 2) {
+        return false
+      }
+
+      if (trimmedName.length > 256) {
+        return false
+      }
+
+      return true
+    }
+}
--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,13 +1,20 @@
 import { Either, left } from '../shared'
 import { Email } from './email'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
+import { Name } from './name'
 import { UserData } from './protocols/user-data'
 
 export class User {
-  static create (userData: UserData): Either<InvalidEmailError, User> {
+  static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
     const emailOrError = Email.create(userData.email)
+    const nameOrError = Name.create(userData.name)
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
+    }
+
+    if (nameOrError.isLeft()) {
+      return left(new InvalidNameError())
     }
   }
 }
