5ace85ef39459d050a582ead47dbaeb1f165ab64
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-19 13:34:08 -0300

    refactor: fix Error types duplicated

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -6,21 +6,24 @@ import { UserRepository } from './ports/user-repository'
 import { Either, left, right } from '../../shared'
 
 export class RegisterUserOnMailingList {
-    private readonly userRepository: UserRepository
-    constructor (userRepository: UserRepository) {
-      this.userRepository = userRepository
+  private readonly userRepository: UserRepository;
+  constructor (userRepository: UserRepository) {
+    this.userRepository = userRepository
+  }
+
+  public async execute (
+    request: UserData
+  ): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> =
+      User.create(request)
+
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
     }
+    if (!(await this.userRepository.exists(request))) {
+      await this.userRepository.add(request)
 
-    public async execute (request: UserData): Promise<Either<InvalidEmailError | InvalidEmailError, UserData>> {
-      const userOrError: Either<InvalidEmailError | InvalidNameError, User> = User.create(request)
-
-      if (userOrError.isLeft()) {
-        return left(userOrError.value)
-      }
-      if (!(await this.userRepository.exists(request))) {
-        await this.userRepository.add(request)
-
-        return right(request)
-      }
+      return right(request)
     }
+  }
 }
