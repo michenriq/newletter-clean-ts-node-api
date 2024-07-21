54d1fc30c5b4d1d9eaab1e7d7b8732f57678c7cb
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-21 15:26:30 -0300

    feat: add cors enabling the middleware

--- src/main/middleware.ts
+++ src/main/middleware.ts
@@ -1,6 +1,7 @@
-import { bodyParser } from '@/main/middlewares'
 import { Express } from 'express'
+import { bodyParser, cors } from '@/main/middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
