598003e78cb545ef5eab976747e8d891c5eec4ac
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-21 15:54:13 -0300

    feat: add content-type middleware

--- src/main/middleware.ts
+++ src/main/middleware.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express'
 import { bodyParser, cors } from '@/main/middlewares'
+import { contentType } from './middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
