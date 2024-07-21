81edefe24d28f582deaa1094f59f7bd811773870
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-21 15:54:30 -0300

    test: add content-type middleware

---
+++ tests/main/middlewares/content-type.test.ts
@@ -0,0 +1,22 @@
+import app from '../../../src/main/config/app'
+import request from 'supertest'
+
+describe('Content-type middleware', () => {
+  test('should receive data as json', async () => {
+    app.post('/test_json_type', (req, res) => {
+      res.type('json')
+      res.send('')
+    })
+
+    await request(app).post('/test_json_type').expect('content-type', /json/)
+  })
+
+  test('should receive data as xml', async () => {
+    app.post('/test_xml_type', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app).post('/test_xml_type').expect('content-type', /xml/)
+  })
+})
