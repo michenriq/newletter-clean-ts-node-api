766a533e461b57fbc9c277427afa1435add5af5b
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-22 14:20:59 -0300

    feat: registerUseController factory

---
+++ src/main/facotires/register.ts
@@ -0,0 +1,16 @@
+import { InMemoryUserRepository } from '../../../tests/usecases/register-user-on-mailing-list/repository'
+import { UserData } from '../../entities/protocols'
+import { RegisterUserOnMailingList } from '../../usecases/register-user-on-mailing-list'
+import { RegisterUserOnMailingListUseCase } from '../../usecases/register-user-on-mailing-list/protocols'
+import { RegisterUserController } from '../../web-controllers/register-user-controller'
+
+export const makeRegisterUserController = (): RegisterUserController => {
+  const users: UserData[] = []
+  const inMemoryuRepository = new InMemoryUserRepository(users)
+  const registerUserOnMKailingListUseCase: RegisterUserOnMailingListUseCase =
+    new RegisterUserOnMailingList(inMemoryuRepository)
+  const registerUserOnMailingListController = new RegisterUserController(
+    registerUserOnMKailingListUseCase
+  )
+  return registerUserOnMailingListController
+}
