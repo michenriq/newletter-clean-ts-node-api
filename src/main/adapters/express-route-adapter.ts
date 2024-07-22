ea846f82687f48b46754457e1525eb6207e14af9
Michel Henrique <michel.henriq.dev@gmail.com>
2024-07-22 14:12:18 -0300

    fix: move middleaawre folder inside config folder

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+import { RegisterUserController } from '../../web-controllers/register-user-controller'
+import { HttpRequest } from '../../web-controllers/protocols'
+
+export const adaptRout = (controller: RegisterUserController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
